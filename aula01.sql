0e41de4dfaf0ab66a80ad83023751fbb005cf6b8
João Pucci <126732888+taldoJonas1@users.noreply.github.com>
2024-10-08 14:17:31 -0300

    Create aula01.sql

---
+++ aula01.sql
@@ -0,0 +1,14 @@
+-- AULA 1: DDL (DATA DEFINITION LANGUAGE) - CREATE | ALTER | DROP
+
+-- CREATE
+CREATE TABLE nome_tabela(
+  ID    BIGINT,
+  nome  VARCHAR(MAX)
+)
+
+-- ALTER
+ALTER TABLE nome_tabela
+ADD hora DATETIME
+
+-- DROP (apaga a tabela)
+DROP TABLE nome_tabela
