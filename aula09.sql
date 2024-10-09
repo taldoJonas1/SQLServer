0038ec0157d6f6e78964191f607562f83f290978
João Pucci <126732888+taldoJonas1@users.noreply.github.com>
2024-10-09 10:20:25 -0300

    Update aula09.sql

--- aula09.sql
+++ aula09.sql
@@ -1,2 +1,10 @@
--- OUTER JOIN
+-- FULL OUTER JOIN
 -- BUSCA TODAS AS INFORAMÇÕES DA TABELA (UNIÃO EM ÁLGEBRA RELACIONAL)
+
+SELECT *
+FROM 
+  tb_clients as c
+FULL OUTER JOIN
+  tb_clients_address as ca
+ON
+  c.client_id = ca.client_id
