ce2f7de9a81692220fca43c71b34f806903cb0e9
João Pucci <126732888+taldoJonas1@users.noreply.github.com>
2024-10-09 10:00:46 -0300

    Create aula08.sql

---
+++ aula08.sql
@@ -0,0 +1,11 @@
+-- LEFT JOIN E RIGHT JOIN
+-- DADAS DUAS TABELAS A e B, LEFT BUSCARÁ TUDO DA A + O QUE FOR CORRESPONDENTE COM B E RIGHT BUSCARÁ TUDO DA B + O QUE FOR CORRESPONDENTE COM A
+
+SELECT *
+FROM tb_clients as c
+LEFT JOIN
+  tb_clients_address as ca
+ON
+  c.client_id = ca.client_id
+WHERE
+  ca.client_id IS NULL
